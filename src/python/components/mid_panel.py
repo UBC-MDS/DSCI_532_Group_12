1e5b660eb8e5540b40d9fdce8e5e19490b2a41f0
Mai Le <lephanthuymai@gmail.com>
2021-01-22 22:22:07 +0700

    Change color scale for worldmap

--- src/python/components/mid_panel.py
+++ src/python/components/mid_panel.py
@@ -32,19 +32,19 @@ class mid_panel(panel):
                             html.Iframe(
                                 id="world_map",
                                 style={
-                                    "border-width" : "0",
+                                    "border-width": "0",
                                     "width": "1000px",
-                                    "height": "1000px"
-                                }
+                                    "height": "1000px",
+                                },
                             )
                         ]
                     ),
-                )
+                ),
             ]
         )
 
     def refresh(self, chart_type="confirmed"):
-        """Aggregate the country level data 
+        """Aggregate the country level data
 
         Args:
             chart_type (string): selected chart type from buttons
@@ -52,25 +52,23 @@ class mid_panel(panel):
         Returns:
             a ranking bar chart of confirmed cases/death cases/recovered cases
         """
-        
-       	# result = self.data_reader.get_aggregated_daily_report()
+
+        # result = self.data_reader.get_aggregated_daily_report()
         confirmed_data = self.data_reader.times_series_confirmed_tidy
         # active_data = result[['Country_Region', 'Active']]
         recovered_data = self.data_reader.times_series_recovered_tidy
         deaths_data = self.data_reader.times_series_death_tidy
-        
+
         if chart_type == "confirmed":
-        	data = confirmed_data
+            data = confirmed_data
         elif chart_type == "recovered":
-        	data = recovered_data
+            data = recovered_data
         elif chart_type == "death":
-        	data = deaths_data
+            data = deaths_data
         # data.columns =['Country_Region', 'Cases']
         chart = self.__create_world_map_chart(data, chart_type.title())
         return chart
- 
-    
-    
+
     def __create_button_groups(self):
         button_groups = dbc.ButtonGroup(
             [
@@ -82,29 +80,33 @@ class mid_panel(panel):
             className="mr-1",
         )
         return button_groups
-    
+
     def __create_world_map_chart(self, data, type):
-        source = alt.topo_feature(dt.world_110m.url, 'countries')
-        base = alt.Chart(source, title = "").mark_geoshape(
-            fill='lightgray',
-            stroke='white'
-            ).properties(
-            width=800,
-            height=450
-            ).project('equirectangular')
-            
-        points = alt.Chart(data[data['variable'] == data['variable'].unique().max()]).mark_circle().encode(
-                longitude='Long:Q',
-                latitude='Lat:Q',
-                size=alt.Size('value:Q', 
-                               title='Number of Confirmed',
-                               scale=alt.Scale(range=[250, 2000]),
-                               legend = None),
-                color=alt.value('steelblue'),
-                tooltip=['Country/Region:N','value:Q']
+        source = alt.topo_feature(dt.world_110m.url, "countries")
+        base = (
+            alt.Chart(source, title="")
+            .mark_geoshape(fill="lightgray", stroke="white")
+            .properties(width=800, height=450)
+            .project("equirectangular")
+        )
+
+        points = (
+            alt.Chart(data[data["variable"] == data["variable"].unique().max()])
+            .mark_circle()
+            .encode(
+                longitude="Long:Q",
+                latitude="Lat:Q",
+                size=alt.Size(
+                    "value:Q",
+                    title="Number of Cases",
+                    scale=alt.Scale(range=[250, 2000]),
+                    # legend=None,
+                ),
+                color=alt.Color("value", scale=alt.Scale(scheme="orangered")),
+                tooltip=["Country/Region:N", "value:Q"],
             )
-        
-        chart = base + points 
-        
+        )
+
+        chart = base + points
+
         return chart.to_html()
-  
